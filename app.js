a5ddaf49ce12ee727d48498a0e1ccec25fc51530
lordkajun6-spec <lordkajun6@gmail.com>
2025-09-06 14:55:59 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ alanlar.forEach(alan => {
     alan.addEventListener('mouseenter', () => {
         const sesAdi = alan.dataset.sound;
         if (sesAdi) {
-            sesOynatici.src = `https://pub-4203e1ea3980402399369ab3ab779e98.r2.dev/sesler/${sesAdi}.wav`; 
+            ses.src = `https://pub-4203e1ea3980402399369ab3ab779e98.r2.dev/sesler/${sesAdi}.wav`; 
             sesOynatici.play();
         }
     });
@@ -16,4 +16,5 @@ alanlar.forEach(alan => {
     });
 
 });
+
 
